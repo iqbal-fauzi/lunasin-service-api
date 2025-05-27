b0aa95aca3c769af3f769c3baa55c75371eb20a4
Iqbal Fauzi <iqbalziiq@gmail.com>
2025-05-27 09:35:48 +0700

    Refactor: Adapt main.ts to be a standard HTTP API for Vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { INestApplication } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // Enable CORS if your frontend is on a different origin
-  app.enableCors();
-  await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+let cachedApp: INestApplication;
+
+export default async function (req, res) {
+  if (!cachedApp) {
+    const expressApp = express();
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+
+    app.setGlobalPrefix('api');
+
+    await app.init();
+    cachedApp = app;
+  }
+
+  const server = cachedApp.getHttpAdapter().getInstance();
+  server(req, res);
 }
-bootstrap();
